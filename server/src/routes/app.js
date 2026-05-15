8ac18471bd47576a61985918a2fd12b0d9622a46
ArtG18 <manosalvagar@gmail.com>
2026-05-14 21:48:33 -0400

    fix: agregar rutas portal appointments y clients

--- server/src/routes/app.js
+++ server/src/routes/app.js
@@ -12,8 +12,13 @@ const workerRoutes            = require('./routes/workers')
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
-app.use(express.json())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://kerlyr-studio-client.onrender.com'  // 
+  ],
+  credentials: true
+}))
 
 // Routes
 app.use('/auth',                authRoutes)
