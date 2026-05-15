83ec2a89118f235569dce57a0d9c488518d735c9
ArtG18 <manosalvagar@gmail.com>
2026-05-15 14:57:06 -0400

    fix: corregir imports admin y quitar whatsapp de portal

--- server/src/routes/appointmentsPortal.js
+++ server/src/routes/appointmentsPortal.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const { PrismaClient } = require('@prisma/client')
-const { confirmationMessage, sendMessage } = require('../services/whatsapp')
-const { formatDate } = require('../utils/helpers')
 
 const router = express.Router()
 const prisma = new PrismaClient()
