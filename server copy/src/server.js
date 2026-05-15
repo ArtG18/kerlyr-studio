fe7c8860d9df135ca020224aa2912e506bbd907f
ArtG18 <manosalvagar@gmail.com>
2026-05-14 20:04:37 -0400

    fix: use env variable for API URL

--- server copy/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const app  = require('./routes/app')
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`🌸 Kerlyr Studio API corriendo en http://localhost:${PORT}`)
-})
